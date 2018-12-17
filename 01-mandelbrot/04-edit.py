2b202854fa112bce9984872d7be3cb9d196da213
Mateusz Konieczny <matkoniecz@gmail.com>
2018-12-17 04:34:48 +0100

    demonstrate difference between x and y coordinate

--- 01-mandelbrot/04-edit.py
+++ 01-mandelbrot/04-edit.py
@@ -4,6 +4,6 @@ height = 200
 # https://pillow.readthedocs.io/en/latest/reference/Image.html
 im = Image.new("RGB", (width, height), (255, 0, 0))
 pixels = im.load()
-pixels[10, 10] = (0, 255, 0)
+pixels[5, 40] = (0, 255, 0)
 im.save("generated.png")
 im.show()
