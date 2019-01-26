2ec0cee66b768d57a28087e6d27dd9075915f274
Mateusz Konieczny <matkoniecz@gmail.com>
2019-01-26 18:59:44 +0100

    expand steps for showing word splitting

---
+++ 03-text-statistics/05-split-words-what-about-other-interpunction.py
@@ -0,0 +1,3 @@
+text = "Ala ma żółtego kota, trzy chomiki i złotą rybkę!"
+text = text.replace(".", "")
+print(text.split(" "))
---
+++ 03-text-statistics/07-split-words-what-about-newline.py
@@ -0,0 +1,17 @@
+import string
+
+text = """Ojcze nasz, któryś jest w niebie
+święć się imię Twoje;
+przyjdź królestwo Twoje;
+bądź wola Twoja jako w niebie tak i na ziemi;
+chleba naszego powszedniego daj nam dzisiaj;
+i odpuść nam nasze winy,
+jako i my odpuszczamy naszym winowajcom;
+i nie wódź nas na pokuszenie;
+ale nas zbaw od złego.
+
+
+Amen."""
+for char in string.punctuation:
+    text = text.replace(char, " ")
+print(text.split(" "))
