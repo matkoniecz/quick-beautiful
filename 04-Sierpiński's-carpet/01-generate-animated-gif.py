067609951c2fdc5499b19063122e3c081a989c5b
Mateusz Konieczny <matkoniecz@gmail.com>
2019-01-28 16:32:29 +0100

    rewrite gif generator

--- 04-Sierpiński's-carpet/01-generate-animated-gif.py
+++ 04-Sierpiński's-carpet/01-generate-animated-gif.py
@@ -1,16 +1,25 @@
 from packaging import version
 from PIL import Image
 
+def save_animated_gif(images, duration):
+    # done using https://pillow.readthedocs.io/en/latest/handbook/image-file-formats.html#saving
+    first_image = images[0]
+    other_images = images[1:]
+    im1.save("out.gif", save_all=True, append_images=other_images, duration=duration)
+
+def check_version():
+    if version.parse(Image.PILLOW_VERSION) >= version.parse("3.4"):
+        return
+    print("Pillow in version not supporting making animated gifs")
+    print("you need to upgrade library version")
+    print("see release notes in")
+    print("https://pillow.readthedocs.io/en/latest/releasenotes/3.4.0.html#append-images-to-gif")
+    raise RuntimeError("upgrade pillow library, to at least 3.4")
+
+check_version()
 width = 300
 height = 300
 im1 = Image.new("RGBA", (width, height), (255, 0, 0))
 im2 = Image.new("RGBA", (width, height), (255, 255, 0))
 im3 = Image.new("RGBA", (width, height), (255, 255, 255))
-if version.parse(Image.PILLOW_VERSION) < version.parse("3.4"):
-    print("Pillow in version not supporting making animated gifs")
-    print("you need to upgrade library version")
-    print("see release notes in")
-    print("https://pillow.readthedocs.io/en/latest/releasenotes/3.4.0.html#append-images-to-gif")
-else:
-    # done using https://pillow.readthedocs.io/en/latest/handbook/image-file-formats.html#saving
-    im1.save("out.gif", save_all=True, append_images=[im2, im3], duration=900)
+save_animated_gif([im1, im2, im3], 900)
