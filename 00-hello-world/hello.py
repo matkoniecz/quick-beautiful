7e785d8391a88f922bb16dca54149666a830c886
Mateusz Konieczny <matkoniecz@gmail.com>
2019-01-28 16:05:24 +0100

    add reminder to the gorst lesson

--- 00-hello-world/hello.py
+++ 00-hello-world/hello.py
@@ -1,3 +1,14 @@
+"""
+introduction to programming
+
+Mention that computers are able to do basic math and do it extremely quickly.
+But it is necessary to precisely describe what exactly and how should be done.
+Surprisingly many things can be done in this way - see existing programs, apps.
+
+But sometimes there is no existing program to do what we want, in such case
+someone needs to write a new program.
+"""
+
 print("hello world!")
 print("2 + 2")
 print(212)
