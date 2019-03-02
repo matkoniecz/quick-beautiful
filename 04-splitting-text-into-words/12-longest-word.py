0480a713dcba0783e2cf2436a7aa0419e9cc9e01
Mateusz Konieczny <matkoniecz@gmail.com>
2019-03-02 13:11:38 +0100

    rewrite text splitter

--- 04-splitting-text-into-words/12-longest-word.py
+++ 04-splitting-text-into-words/12-longest-word.py
@@ -4,19 +4,22 @@ def text_into_word_array(text):
     return text.split()
 
 
-text = "Ala ma kota. Marysia ma rysia. Czyli Marysia też ma kota."
+def show_longest_word(text):
+    print("longest word in " + text)
+    words = text_into_word_array(text)
+    if len(words) == 0:
+        print("no words in text")
+        return
+    longest_word = words[0]
+    for word in text_into_word_array(text):
+        print(word)
+        print(len(word))
+        if len(longest_word) < len(word):
+            print("new longest word")
+            longest_word = word
+        print()
+    print(longest_word)
 
-
-print("longest word")
-longest_word = None
-longest_word_length = 0
-for word in text_into_word_array(text):
-    print(word)
-    print(len(word))
-    if longest_word_length < len(word):
-        print("new longest word")
-        longest_word = word
-        longest_word_length = len(word)
-    print()
-
-print()
+show_longest_word("Ala ma kota. Marysia ma rysia. Czyli Marysia też ma kota.")
+show_longest_word("!!!!!!!!!!")
+show_longest_word("Aaaaaaaaaaaaaaaaaaaaaaaaby żółwia sprzedam")
