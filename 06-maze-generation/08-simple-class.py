87d5e1d412136e6d0fdd8c1bc862e73159672971
Mateusz Konieczny <matkoniecz@gmail.com>
2019-05-11 09:36:43 +0200

    add simple class example

---
+++ 06-maze-generation/08-simple-class.py
@@ -0,0 +1,12 @@
+class Dog():
+    __init__(self, name):
+        self.name = name
+
+    def make_noise():
+        print(name + ": woof, woof!")
+
+    def jump():
+        print(name + " jumps")
+
+    def sleep():
+        print(name + " sleeps")
