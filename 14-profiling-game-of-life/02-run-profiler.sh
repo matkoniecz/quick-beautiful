65e7059ef70636535973db8e0cf40ec86aee4ce5
Mateusz Konieczny <matkoniecz@gmail.com>
2020-07-31 07:35:14 +0200

    update about profilers as of 2020

--- 14-profiling-game-of-life/02-run-profiler.sh
+++ 14-profiling-game-of-life/02-run-profiler.sh
@@ -2,8 +2,8 @@ py-spy --flame before.svg -d 10 -- python3 01-pygame-game-of-life-limited-iterat
 py-spy --flame after.svg -d 10 -- python3 03-pygame-game-of-life-optimized-limited-iterations.py
 py-spy --flame without_white_on_white.svg -d 10 -- python3 04-stop-double-drawing.py
 
-vprof has bad performance and some interface issues like https://github.com/nvdv/vprof/issues/91 or https://github.com/nvdv/vprof/issues/90
-pyflame is harder to setup and run and I see no benefits over py-spy
-tuna can be run once - https://github.com/nschloe/tuna/issues/44
-snakeviz shows blatantly wrong statistics - https://github.com/jiffyclub/snakeviz/issues/112
-note also poor man's sampling profiler - https://stackoverflow.com/questions/375913/how-can-i-profile-c-code-running-on-linux/378024#378024
+# note also poor man's sampling profiler - https://stackoverflow.com/questions/375913/how-can-i-profile-c-code-running-on-linux/378024#378024
+# tuna is a potential alternative
+# vprof has bad performance and major unfixed interface issues like https://github.com/nvdv/vprof/issues/91 or https://github.com/nvdv/vprof/issues/90
+# pyflame project died. It was anyway harder to setup and run, without benefits over py-spy
+# snakeviz shows blatantly wrong statistics, what was wontfixed https://github.com/jiffyclub/snakeviz/issues/112
