d8420d67b44e03e5f68eb9e2182cbd038cbe6069
Mateusz Konieczny <matkoniecz@gmail.com>
2021-03-10 11:06:07 +0100

    document Tuna

--- 14-profiling-game-of-life/02-run-profiler.sh
+++ 14-profiling-game-of-life/02-run-profiler.sh
@@ -3,7 +3,13 @@ py-spy record --output after.svg --duration 10 -- python3 03-pygame-game-of-life
 py-spy record --output without_white_on_white.svg --duration 10 -- python3 04-stop-double-drawing.py
 
 # note also poor man's sampling profiler - https://stackoverflow.com/questions/375913/how-can-i-profile-c-code-running-on-linux/378024#378024
+
 # tuna is a potential alternative
+# Create a runtime profile with
+# python3 -mcProfile -o program.prof yourfile.py
+# and show it with
+# tuna program.prof
+
 # vprof has bad performance and major unfixed interface issues like https://github.com/nvdv/vprof/issues/91 or https://github.com/nvdv/vprof/issues/90
 # pyflame project died. It was anyway harder to setup and run, without benefits over py-spy
 # snakeviz shows blatantly wrong statistics, what was wontfixed https://github.com/jiffyclub/snakeviz/issues/112
