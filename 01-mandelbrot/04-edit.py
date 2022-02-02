c7735f006daffb324f96501ce004445f08799dce
Mateusz Konieczny <matkoniecz@gmail.com>
2022-02-02 19:14:56 +0100

    indicate indexing

--- 01-mandelbrot/04-edit.py
+++ 01-mandelbrot/04-edit.py
@@ -4,8 +4,8 @@ height = 200
 # https://pillow.readthedocs.io/en/latest/reference/Image.html
 im = Image.new("RGB", (width, height), (255, 0, 0))
 pixels = im.load()
-x = 5
-y = 40
+x = 5 # indexed from left
+y = 40 # indexed from top
 pixels[x, y] = (0, 255, 0)
 im.save("generated.png")
 im.show()
